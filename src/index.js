3140fb1e745c75ffc9da93a13de941f37581bcfc
mishra-anant <mishra.anant.hbtu2k23@gmail.com>
2025-04-04 18:14:22 +0530

    Star rating component fully made

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 //import "./index.css";
 //import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        maxRating={10}
+        onSetRating={setMovieRating}
+      ></StarRating>
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<App />);
 root.render(
   <>
-    <StarRating maxRating={5}></StarRating>
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "okay", "Good", "Amazing"]}
+    ></StarRating>
+    <StarRating color="red" className="test" defaultRating={3}></StarRating>
+    <Test></Test>
   </>
 );
-                                                                                
